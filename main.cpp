a1643e641da7de2f0cdf2543e5af4b2071eb2218
pf packet <afpacket@gmail.com>
2012-03-30 19:15:24 +0900

    Made simple

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <stdexcept>
 #include <boost/asio.hpp>
@@ -9,7 +8,6 @@
 #include "raw_tcp.hpp"
 #include "tcp_header.hpp"
 
-// Return resolved string of IP address by using template parameter, protocol
 template< typename protocol = boost::asio::ip::tcp >
 std::string hostname_resolver(const char* hostname, std::string hint = "")
 {
@@ -18,15 +16,6 @@ std::string hostname_resolver(const char* hostname, std::string hint = "")
     typename protocol::resolver::query squery(hostname, hint.c_str());
     typename protocol::resolver::iterator it = resolver.resolve(squery);
     return it->endpoint().address().to_string();
-}
-
-bool running = true;
-
-void sighandler(const boost::system::error_code &error, int signo, bool &flag) {
-    if( error )
-        return;
-    std::cout << " [*] Stop working" << std::endl;
-    flag = false;
 }
 
 int main(int argc, char **argv)
@@ -38,18 +27,15 @@ int main(int argc, char **argv)
             throw std::string("Arguments are too few");
 
         std::cout << "TCP packet with SYN flag: " << argv[1] << " -> " << argv[2] << ':' << argv[3] << " times=" << argv[4] << std::endl;
-        // Resolve hostname by using boost::asio::ip::raw_tcp
         std::string result = hostname_resolver<boost::asio::ip::raw_tcp>(argv[2]);
         std::cout << "hostname: " << argv[2] << "=" << result << std::endl;
         
-        // Make endpoint of boost::asio::ip::ether
         boost::asio::io_service io_service;
         boost::asio::ip::raw_tcp::socket socket(io_service, boost::asio::ip::raw_tcp::v4());
         boost::asio::ip::raw_tcp::resolver resolver(io_service);
         boost::asio::ip::raw_tcp::resolver::query query(boost::asio::ip::raw_tcp::v4(), argv[2], "");
         boost::asio::ip::raw_tcp::endpoint destination = *resolver.resolve(query);
 
-        // Create syn packets and send them to the target
         boost::asio::streambuf request_buffer;
         std::ostream os(&request_buffer);
         tcp_header syn_packet(argv[1], result);
@@ -61,9 +47,8 @@ int main(int argc, char **argv)
         syn_packet.window(32792);
         os << syn_packet;
         int num = atoi(argv[4]);
-        for( ; running && i <= num; ++i ) {
+        for( ; i <= num; ++i ) {
             socket.send_to(request_buffer.data(), destination);
-            // "\033[2K" - Escape Sequence to move the cursor to the begin of line
             std::cout << "\033[100D" << "[*] Sent TCP(SYN) packet to " << result << " seq=" << i << std::flush;
             io_service.run();
         }
