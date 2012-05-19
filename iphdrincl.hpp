b71d2868505a519d48a00b2bcd3c3451ca0ea698
pf packet <afpacket@gmail.com>
2012-05-19 14:57:12 +0900

    Add iphdrincl.hpp to enable IP_HDRINCL socket option, but it's not used.

---
+++ iphdrincl.hpp
@@ -0,0 +1,43 @@
+//
+// ip_hdrincl.hpp
+// Enable IP_HDRINCL of raw socket
+// This is available only for basic_raw_socket
+//
+#ifndef BOOST_ASIO_IP_HDRINCL_SOCKOPT
+#define BOOST_ASIO_IP_HDRINCL_SOCKOPT
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+namespace boost {
+namespace asio {
+namespace ip {
+    
+// This meets SettableSocketOption requirement
+// See the documents of Boost.Asio for more information
+class ip_hdrincl {
+public:
+    ip_hdrincl() : one(1) {}
+    ~ip_hdrincl() {}
+
+    template<typename Protocol>
+    int level(Protocol p) const { return IPPROTO_IP; }
+
+    template<typename Protocol>
+    int name(Protocol p)  const { return IP_HDRINCL; }
+
+    template<typename Protocol>
+    const void *data(Protocol p) const { return reinterpret_cast<const void*>(&one); }
+
+    template<typename Protocol>
+    int size(Protocol p) const { return sizeof(one); }
+private:
+    int one;
+};
+
+} // namespace ip
+} // namespace asio
+} // namespace boost
+
+#endif // BOOST_ASIO_IP_HDRINCL_SOCKOPT
+
