fcb2b6718367cf8ca197ce8a6c882e9ca51765b7
pf packet <afpacket@gmail.com>
2012-06-16 14:58:04 +0900

    Use standard typedef fixed types

--- src/tcp_header.hpp
+++ src/tcp_header.hpp
@@ -65,39 +65,39 @@ public:
     explicit tcp_header(const header_type &tcph) : rep_(tcph) {}
     ~tcp_header() {}
 
-    unsigned char source() const { return ntohs(rep_.source); }
-    unsigned char dest() const { return ntohs(rep_.dest); }
-    unsigned int seq() const { return ntohl(rep_.seq); }
-    unsigned int ack_seq() const { return ntohl(rep_.ack_seq); }
-    unsigned short res1() const { return rep_.res1; }
-    unsigned short doff() const { return rep_.doff; }
-    unsigned short fin() const { return rep_.fin; }
-    unsigned short syn() const { return rep_.syn; }
-    unsigned short rst() const { return rep_.rst; }
-    unsigned short psh() const { return rep_.psh; }
-    unsigned short ack() const { return rep_.ack; }
-    unsigned short urg() const { return rep_.urg; }
-    unsigned short res2() const { return rep_.res2; }
-    unsigned short window() const { return ntohs(rep_.window); }
-    unsigned short check() const { return ntohs(rep_.check); }
-    unsigned short urg_ptr() const { return ntohs(rep_.urg_ptr); }
+    uint16_t source() const { return ntohs(rep_.source); }
+    uint16_t dest() const { return ntohs(rep_.dest); }
+    uint32_t seq() const { return ntohl(rep_.seq); }
+    uint32_t ack_seq() const { return ntohl(rep_.ack_seq); }
+    uint16_t res1() const { return rep_.res1; }
+    uint16_t doff() const { return rep_.doff; }
+    uint16_t fin() const { return rep_.fin; }
+    uint16_t syn() const { return rep_.syn; }
+    uint16_t rst() const { return rep_.rst; }
+    uint16_t psh() const { return rep_.psh; }
+    uint16_t ack() const { return rep_.ack; }
+    uint16_t urg() const { return rep_.urg; }
+    uint16_t res2() const { return rep_.res2; }
+    uint16_t window() const { return ntohs(rep_.window); }
+    uint16_t check() const { return ntohs(rep_.check); }
+    uint16_t urg_ptr() const { return ntohs(rep_.urg_ptr); }
 
-    void source(unsigned short source) { rep_.source = htons(source); }
-    void dest(unsigned short dest) { rep_.dest = htons(dest); }
-    void seq(unsigned int seq) { rep_.seq = htonl(seq); }
-    void ack_seq(unsigned int ack_seq) { rep_.ack_seq = htonl(ack_seq); }
-    void res1(unsigned short res1) { rep_.res1 = res1; }
-    void doff(unsigned short doff) { rep_.doff = doff; }
+    void source(uint16_t source) { rep_.source = htons(source); }
+    void dest(uint16_t dest) { rep_.dest = htons(dest); }
+    void seq(uint32_t seq) { rep_.seq = htonl(seq); }
+    void ack_seq(uint32_t ack_seq) { rep_.ack_seq = htonl(ack_seq); }
+    void res1(uint16_t res1) { rep_.res1 = res1; }
+    void doff(uint16_t doff) { rep_.doff = doff; }
     void fin(bool fin) { rep_.fin = (fin) ? 1 : 0; }
     void syn(bool syn) { rep_.syn = (syn) ? 1 : 0; }
     void rst(bool rst) { rep_.rst = (rst) ? 1 : 0; }
     void psh(bool psh) { rep_.psh = (psh) ? 1 : 0; }
     void ack(bool ack) { rep_.ack = (ack) ? 1 : 0; }
-    void urg(unsigned short urg) { rep_.urg = urg; }
-    void res2(unsigned short res2) { rep_.res2 = res2; }
-    void window(unsigned short window) { rep_.window = htons(window); }
-    void check(unsigned short check) { rep_.check = htons(check); }
-    void urg_ptr(unsigned short urg_ptr) { rep_.urg_ptr = htons(urg_ptr); }
+    void urg(uint16_t urg) { rep_.urg = urg; }
+    void res2(uint16_t res2) { rep_.res2 = res2; }
+    void window(uint16_t window) { rep_.window = htons(window); }
+    void check(uint16_t check) { rep_.check = htons(check); }
+    void urg_ptr(uint16_t urg_ptr) { rep_.urg_ptr = htons(urg_ptr); }
 
     int length() const { return sizeof(rep_); }
     char* get_header() { return reinterpret_cast<char*>(&rep_); }
@@ -112,7 +112,7 @@ public:
         tc.pseudo.protocol = IPPROTO_TCP;
         tc.pseudo.length   = htons(sizeof(tcphdr));
         tc.tcphdr = rep_;
-        rep_.check = ((checksum(reinterpret_cast<unsigned short*>(&tc), sizeof(struct tcp_checksum))));
+        rep_.check = ((checksum(reinterpret_cast<uint16_t*>(&tc), sizeof(struct tcp_checksum))));
     }
 
     void compute_checksum(const std::string &srcaddr, const std::string &destaddr) {
